1bfac21298112f45269de14b1c17a8ddb871bab2
BylatZM <ghost.stop@yandex.ru>
2024-01-22 12:35:31 +0300

    deploy fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Authentication } from './components/authentication/Authentication'
 import { Loading } from './components/loading/Loading'
