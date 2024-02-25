9f4c0ef38c77549d0bb3a0fe8e0a176bcb08b75c
BylatZM <ghost.stop@yandex.ru>
2024-02-25 13:23:00 +0300

    added proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,16 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://95.214.11.83:8080',
+      changeOrigin: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+      }
+    })
+  );
+};
